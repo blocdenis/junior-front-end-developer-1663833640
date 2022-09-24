750767b0719d90581e919142fac665a62eb4da23
denis <denis@macbook-air-denis-2.home>
2022-09-24 21:00:14 +0200

    created mocked  data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+let data = [
+  {
+    color: "green",
+    title: "Application setup",
+    buisnes_context: [],
+    status: "completed",
+  },
+  {
+    color: "green",
+    title: "static books list",
+    buisnes_context: [],
+    status: "completed",
+  },
+  {
+    color: "green",
+    title: "Administration panel",
+    buisnes_context: [],
+    status: "completed",
+  },
+  {
+    color: "blue",
+    title: "Connect Admin with Frontend ",
+    buisnes_context: [],
+    status: "active",
+  },
+  {
+    color: "grey",
+    title: "Book review Feature",
+    buisnes_context: [],
+    status: "blocked",
+  },
+];
